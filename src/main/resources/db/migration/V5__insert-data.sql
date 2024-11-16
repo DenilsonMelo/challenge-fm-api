f2396af435366c34c6b7255bc8b249f7768c7d35
DenilsonMelo <denilsonmlo42@gmail.com>
2024-11-16 09:46:08 -0300

    feat: configuração do docker e script de inserção de dados

---
+++ src/main/resources/db/migration/V5__insert-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO car (model, type) VALUES ('Perua', 'Van'), ('Fiorino', 'Van'), ('Caminhão X', 'Caminhão'), ('Van 2', 'Van');
+
+INSERT INTO driver (name) VALUES ('Pedro Augusto'), ('Carlos'), ('João'), ('Lucia');
+
+INSERT INTO carrier (name) VALUES ('Log X'), ('Express'), ('Log Log'), ('Juarez Express');
+
+INSERT INTO package (type) VALUES ('Caixa'), ('Volume plástico');
+
+INSERT INTO payment (type, amount) VALUES ('Dinheiro', '150'), ('Dinheiro', '50'), ('Cartão X', '120'), ('Cheque', '600');
